4d9d1c573233674d5889cd30dda3a18a003cd25a
mean <fixounet@free.fr>
2021-03-31 07:31:03 +0200

    import

---
+++ ssd1306_i2c_f1.h
@@ -0,0 +1,17 @@
+#include "Wire.h"
+#include "ssd1306_base.h"
+
+class OLED_stm32duino : public  OLEDCore
+{
+    public:
+                OLED_stm32duino(WireBase &wire, int reset);
+        void    sendCommand(uint8_t cmd);
+        void    update();
+        //--
+        void    beginData();
+    protected:
+        WireBase &_wire;
+    
+};
+
+
